e62ad15f2d7b13a08517d167d69c6f36a7c2a349
Richard Patching <richard@justaddpixels.com>
2021-01-19 16:33:22 +0000

    Populate stats on guild member click

--- Scripts/CreateParty.gd
+++ Scripts/CreateParty.gd
@@ -1,17 +1,32 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var party_members = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func init():
-	pass
+	party_members = []
+	$PartyMembers.clear()
+	$GuildMembers.clear()
+	
+	for id in GameData.data.player.guild.recruits:
+		var recruit = GameData.data.npcs[id]
+		$GuildMembers.add_item(recruit.name)
+		party_members.push_back(id)
 
 func update_ui():
 	pass
+
+
+func _on_GuildMembers_item_selected(index):
+	var id = party_members[index]
+	var recruit = GameData.data.npcs[id]
+	$NameLabel.text = recruit.name.to_upper()
+	$Might.text = str(recruit.might)
+	$Mind.text = str(recruit.mind)
+	$Constitution.text = str(recruit.constitution)
+	$Charisma.text = str(recruit.charisma)
+	$Reflex.text = str(recruit.reflex)
+	$Level.text = str(recruit.level)
